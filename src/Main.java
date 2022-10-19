32c96f299c65443248963f6b2ea9552d9c73191d
Rumonz <lone_assassin@mail.ru>
2022-10-20 02:52:00 +0300

    Посчитайте сколько часов работы должно быть поделено между сотрудниками, если в компании работает на 94 человека больше. Выведите результат задачи в консоль в формате "Если в компании работает … человек, то всего … часов работы может быть поделено между сотрудниками".

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
       var onePersonUsedTime = 8;
       var totalPersons = totalTime / onePersonUsedTime;
         System.out.println("Всего работников в компании " + totalPersons + " человек");
+        var extraPersons = 94;
+        totalPersons = totalPersons + extraPersons;
+        System.out.println(totalPersons);
+       totalTime= totalPersons * onePersonUsedTime;
+        System.out.println("Если в компании работает "+totalPersons+ " человек, то всего "+totalTime+ " часов работы может быть поделено между сотрудниками");
     }
 }
